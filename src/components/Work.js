ff11c2c5e3048c3bbccf14763cf47968b05c75cb
santibrizuela <santibrizuela.97@gmail.com>
2024-03-15 18:45:16 -0300

    Divano Link changeeeeewwww

--- src/components/Work.js
+++ src/components/Work.js
@@ -92,7 +92,7 @@ const Work = () => {
             {/* pretitle */}
             <div className='absolute -bottom-full left-12 
             group-hover:bottom-24 transition-all duration-500 z-50'>
-              <span className='text-gradient'>Desarrollos Frontend y de Marca</span>             
+              <span className='text-gradient'>Desarrollo Frontend y de Marca</span>             
             </div>
             {/* title */}
             <div className='absolute -bottom-full left-12 
